e8cbd38c73378fafdccbc13c7ba1572eb02f0959
pedro_dos_santos <pedrohenriq1919@gmail.com>
2026-08-14 19:05:42 -0300

    Primeiros cálculos usando PY (Operações e Frações)

---
+++ fundamentos/operacoes_basicas.py
@@ -0,0 +1,28 @@
+#Definicao das variaveis
+a = 20
+b = 6
+
+#Operacoes basicas
+divisao = a / b
+divisao_inteira = a // b
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+potencia = a**b
+resto = a % b
+
+#Mostrar resultado
+print (resto)
+
+#Testando input e tipo de dados
+c = int(input("Insira o valor de c: "))
+print(c)
+
+d = int(input("Insira o valor de d: "))
+print(d)
+
+resultado = c + d + 2
+print(f"Aqui está seu resultado: {resultado}")
+
+#Utilizando operadores relacionais
+
